9b3eee8213ca06ed140e8545ad6f38f94757571e
Anbarasu Angappan <anburvhs@hotmail.com>
2018-06-21 17:05:34 +0530

    Add project files.

---
+++ WpfAppMVVM1/Models/TestModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace WpfAppMVVM1.Models
+{
+    class TestModel : BindableBase
+    {
+        public string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                SetProperty(ref _name, value);
+            }
+        }
+
+
+        public string _qualification;
+        public string Qualification
+        {
+            get
+            {
+                return _qualification;
+            }
+            set
+            {
+                SetProperty(ref _qualification, value);
+            }
+        }
+
+        public string _message;
+        public string Message
+        {
+            get
+            {
+                return _message;
+            }
+            set
+            {
+                SetProperty(ref _message, value);
+            }
+        }
+    }    
+}
---
+++ WpfAppMVVM1/ViewModels/TestViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfAppMVVM1.Models;
+using Prism.Mvvm;
+using System.Windows.Input;
+using Prism.Commands;
+
+namespace WpfAppMVVM1.ViewModels
+{
+    class TestViewModel : BindableBase
+    {
+        private TestModel testModel;        
+        public ICommand iclickcommand
+        {
+            get;
+            set;
+        }
+
+        public TestViewModel()
+        {
+            testModel = new TestModel();             
+            TestModel1("SAI","CSE");// testModel = new TestModel("SAI","CSE"); 
+
+            //testModel1 = (new TestModel() { Name = "", Qualification = "" });
+
+            iclickcommand = new DelegateCommand(ClickedMethod);
+        }
+
+     
+
+        public TestModel TestModel
+        {
+            get
+            {
+                return testModel;
+            }
+            set
+            {
+                SetProperty(ref testModel, value);
+            }
+        }
+
+        private void ClickedMethod()
+        {
+            TestModel.Message = "You Have Clicked the button";
+        }
+
+        public void TestModel1(string Names, string Qualifications)
+        {
+            try
+            {
+                if (Names != null)
+                {
+                    testModel.Name = Names;
+                    testModel.Qualification = Qualifications;
+                }
+                else
+                {
+                    //return null;
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+    }
+
+}
